56ef0c74bbee814e8976d5537cdf260b50362bfd
leodotng <josh@leo.ng>
2020-03-26 12:07:39 -0600

    smaller input fields

--- src/App.js
+++ src/App.js
@@ -13,8 +13,7 @@ import {
   InputGroupText,
   InputGroupAddon
 } from "shards-react";
-import Clock from 'react-live-clock';
-
+import Clock from "react-live-clock";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import "shards-ui/dist/css/shards.min.css";
@@ -26,23 +25,35 @@ function App() {
         <CardBody>
           <Card className="card-spacing">
             <CardBody>
-              <h1>WhenIsBriefing.com ?</h1>
-              <h3>Current Time: <Clock format={'HH:mm:ss'} ticking={true} timezone={'US/Mountain'} /></h3>
-              
+              <h2>WhenIsBriefing.com ?</h2>
+              <h3>
+                Current Time:{" "}
+                <Clock
+                  format={"HH:mm:ss"}
+                  ticking={true}
+                  timezone={"US/Mountain"}
+                />
+              </h3>
             </CardBody>
           </Card>
           <Card className="card-spacing">
             <CardBody>
               <Form>
-                <FormGroup size="lg">
+                <FormGroup size="sm">
                   <InputGroup>
-                  <InputGroupAddon type="prepend">
-                    <InputGroupText>Above The Wing:</InputGroupText>
-                  </InputGroupAddon>
-                  {/* <label htmlFor="#AboveTheWing">Above The Wing: </label> */}
-                  <FormInput id="#AboveTheWing" placeholder="Above The Wing" />
-                  <Button size="large"> Update </Button>
-                  <br />
+                    <InputGroupAddon type="prepend" size="sm">
+                      <InputGroupText size="sm">Above The Wing:</InputGroupText>
+                    </InputGroupAddon>
+                    {/* <label htmlFor="#AboveTheWing">Above The Wing: </label> */}
+                    <FormInput
+                      type="time"
+                      min="09:00"
+                      max="18:00"
+                      id="#AboveTheWing"
+                      placeholder="09:00"
+                    />
+                    <Button size="sm"> Update </Button>
+                    <br />
                   </InputGroup>
                 </FormGroup>
               </Form>
@@ -51,35 +62,39 @@ function App() {
           <Card className="card-spacing">
             <CardBody>
               <Form>
-                <FormGroup size="lg">
+                <FormGroup size="sm">
+                  <InputGroup size="sm">
+                    <InputGroupAddon type="prepend" size="sm">
+                      <InputGroupText>Below The Wing:</InputGroupText>
+                    </InputGroupAddon>
+                    {/* <label htmlFor="#BelowTheWing">Below The Wing:</label> */}
+                    <FormInput
+                      type="time"
+                      min="09:00"
+                      max="18:00"
+                      id="#BelowTheWing"
+                      placeholder="09:15"
+                    />
+                    <Button size="sm"> Update </Button>
+                    <br />
+                  </InputGroup>
+                </FormGroup>
+              </Form>
+            </CardBody>
+          </Card>
+          <Card className="card-spacing">
+            <CardBody>
+              <Form>
                 <InputGroup>
                   <InputGroupAddon type="prepend">
-                    <InputGroupText>Below The Wing:</InputGroupText>
+                    <InputGroupText>Location:</InputGroupText>
                   </InputGroupAddon>
-                  {/* <label htmlFor="#BelowTheWing">Below The Wing:</label> */}
-                  <FormInput id="#BelowTheWing" placeholder="Below The Wing" />
-                  <Button size="large"> Update </Button>
-                  <br />
-                  </InputGroup>
-                </FormGroup>
-              </Form>
-            </CardBody>
-          </Card>
-          <Card className="card-spacing">
-            <CardBody>
-              
-              <Form>
-              <InputGroup>
-                  <InputGroupAddon type="prepend">
-                    <InputGroupText>Above The Wing:</InputGroupText>
-                  </InputGroupAddon>
-                <FormSelect size="lg">
-                  <option value="Training Room 3">Training Room 3</option>
-                  <option value="Training Room 2">Training Room 2</option>
-                </FormSelect>
-                <Button size="large">Update</Button>
+                  <FormSelect size="sm">
+                    <option value="Training Room 3">Training Room 3</option>
+                    <option value="Training Room 2">Training Room 2</option>
+                  </FormSelect>
+                  <Button size="sm">Update</Button>
                 </InputGroup>
-                
               </Form>
             </CardBody>
           </Card>
